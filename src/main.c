7f2187725ef69f51e7fd1d58c754f2ed225131cc
Angry Wasp <angrywasp@live.com>
2022-08-08 21:02:04 +1000

    Update main

--- src/main.c
+++ src/main.c
@@ -10,8 +10,27 @@
 
 volatile io_binding_t *adc_binding7;
 volatile io_binding_t *adc_binding8;
-volatile io_binding_t *btn_binding;
-volatile io_binding_t* bindings[3] = {0};
+volatile io_binding_t *btn_binding1;
+volatile io_binding_t *btn_binding2;
+volatile io_binding_t *btn_binding3;
+volatile io_binding_t *btn_binding4;
+volatile io_binding_t *btn_binding5;
+volatile io_binding_t *btn_binding6;
+volatile io_binding_t *btn_binding7;
+volatile io_binding_t *btn_binding8;
+volatile io_binding_t *btn_binding9;
+volatile io_binding_t *btn_binding10;
+volatile io_binding_t *btn_binding11;
+volatile io_binding_t *btn_binding12;
+volatile io_binding_t *btn_binding13;
+volatile io_binding_t *btn_binding14;
+volatile io_binding_t *btn_binding15;
+volatile io_binding_t *btn_binding16;
+volatile io_binding_t *btn_binding17;
+volatile io_binding_t *btn_binding18;
+volatile io_binding_t *btn_binding19;
+volatile io_binding_t *btn_binding20;
+volatile io_binding_t* bindings[22] = {0};
 
 void adc();
 void io();
@@ -25,19 +44,56 @@ int main()
 #endif
     pause(100);
 
-    for (int i = 4; i <= 19; i++)
+    for (int i = 4; i <= 23; i++)
         dir_in(i);
 
-    for (int i = 20; i <= 27; i++)
-        dir_out(i);
+    adc_binding7 = io_add_binding(ad7812_A7, "LEFT_DDI_BRT_CTL %d\n");
+    adc_binding8 = io_add_binding(ad7812_A8, "LEFT_DDI_CONT_CTL %d\n");
 
-    adc_binding7 = io_add_binding(ad7812_A7, "ADC7 %d\n");
-    adc_binding8 = io_add_binding(ad7812_A8, "ADC8 %d\n");
-    btn_binding = io_add_binding(4, "BTN %d\n");
+    btn_binding1 = io_add_binding(4, "LEFT_DDI_PB_01 %d\n");
+    btn_binding2 = io_add_binding(5, "LEFT_DDI_PB_02 %d\n");
+    btn_binding3 = io_add_binding(6, "LEFT_DDI_PB_03 %d\n");
+    btn_binding4 = io_add_binding(7, "LEFT_DDI_PB_04 %d\n");
+    btn_binding5 = io_add_binding(8, "LEFT_DDI_PB_05 %d\n");
+    btn_binding6 = io_add_binding(9, "LEFT_DDI_PB_06 %d\n");
+    btn_binding7 = io_add_binding(10, "LEFT_DDI_PB_07 %d\n");
+    btn_binding8 = io_add_binding(11, "LEFT_DDI_PB_08 %d\n");
+    btn_binding9 = io_add_binding(12, "LEFT_DDI_PB_09 %d\n");
+    btn_binding10 = io_add_binding(13, "LEFT_DDI_PB_10 %d\n");
+    btn_binding11 = io_add_binding(14, "LEFT_DDI_PB_11 %d\n");
+    btn_binding12 = io_add_binding(15, "LEFT_DDI_PB_12 %d\n");
+    btn_binding13 = io_add_binding(16, "LEFT_DDI_PB_13 %d\n");
+    btn_binding14 = io_add_binding(17, "LEFT_DDI_PB_14 %d\n");
+    btn_binding15 = io_add_binding(18, "LEFT_DDI_PB_15 %d\n");
+    btn_binding16 = io_add_binding(19, "LEFT_DDI_PB_16 %d\n");
+    btn_binding17 = io_add_binding(20, "LEFT_DDI_PB_17 %d\n");
+    btn_binding18 = io_add_binding(21, "LEFT_DDI_PB_18 %d\n");
+    btn_binding19 = io_add_binding(22, "LEFT_DDI_PB_19 %d\n");
+    btn_binding20 = io_add_binding(23, "LEFT_DDI_PB_20 %d\n");
 
     bindings[0] = adc_binding7;
     bindings[1] = adc_binding8;
-    bindings[2] = btn_binding;
+
+    bindings[2] = btn_binding1;
+    bindings[3] = btn_binding2;
+    bindings[4] = btn_binding3;
+    bindings[5] = btn_binding4;
+    bindings[6] = btn_binding5;
+    bindings[7] = btn_binding6;
+    bindings[8] = btn_binding7;
+    bindings[9] = btn_binding8;
+    bindings[10] = btn_binding9;
+    bindings[11] = btn_binding10;
+    bindings[12] = btn_binding11;
+    bindings[13] = btn_binding12;
+    bindings[14] = btn_binding13;
+    bindings[15] = btn_binding14;
+    bindings[16] = btn_binding15;
+    bindings[17] = btn_binding16;
+    bindings[18] = btn_binding17;
+    bindings[19] = btn_binding18;
+    bindings[20] = btn_binding19;
+    bindings[21] = btn_binding20;
 
     pause(100);
     cog_run(adc, 128);
@@ -45,14 +101,11 @@ int main()
     pause(100);
     cog_run(io, 128);
 
-    pause(100);
-    cog_run(led, 128);
-
     pause(1000);
 
     while(true)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 22; i++)
         {
             if (!bindings[i]->newValueReady)
                 continue;
@@ -82,28 +135,25 @@ void io()
 {
     while (true)
     {
-        io_switch(btn_binding);
-    }
-}
-
-void led()
-{
-    while (true)
-    {
-        if (!adc_binding7->newValueReady)
-            continue;
-
-        if (!adc_binding7->changed)
-            continue;
-
-        int v = adc_binding7->lastVal;
-        ++v;
-        v /= 32;
-
-        for (int i = 20; i <= 27; i++)
-            lo(i);
-
-        for (int i = 0; i < v; i++)
-            hi(20 + i);
+        io_switch(btn_binding1);
+        io_switch(btn_binding2);
+        io_switch(btn_binding3);
+        io_switch(btn_binding4);
+        io_switch(btn_binding5);
+        io_switch(btn_binding6);
+        io_switch(btn_binding7);
+        io_switch(btn_binding8);
+        io_switch(btn_binding9);
+        io_switch(btn_binding10);
+        io_switch(btn_binding11);
+        io_switch(btn_binding12);
+        io_switch(btn_binding13);
+        io_switch(btn_binding14);
+        io_switch(btn_binding15);
+        io_switch(btn_binding16);
+        io_switch(btn_binding17);
+        io_switch(btn_binding18);
+        io_switch(btn_binding19);
+        io_switch(btn_binding20);
     }
 }
