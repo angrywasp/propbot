d566d62000fb60cbac915587a7215f0d5bf24cf3
angrywasp <angrywasp@live.com>
2022-10-01 23:59:04 +1000

    propbot tx update

--- src/propbotTX.c
+++ src/propbotTX.c
@@ -24,23 +24,23 @@ static byte *rf_addr = (byte *)"pbot";
 #define ADC_CLK 1
 #define ADC_CONVST 0
 
-#define OLED_SCL 6
-#define OLED_SDA 5
+#define OLED_SCL 22
+#define OLED_SDA 23
 
-#define NRF_MOSI 20
-#define NRF_MISO 16
-#define NRF_CLK 17
-#define NRF_CS 21
-#define NRF_CE 18
+#define NRF_MOSI 10
+#define NRF_MISO 11
+#define NRF_CLK 12
+#define NRF_CS 9
+#define NRF_CE 13
 
 #define BATTERY_PORT ad7812_A2
 #define JOY_LEFT_X_PORT ad7812_A3
 #define JOY_LEFT_Y_PORT ad7812_A4
-#define JOY_RIGHT_X_PORT ad7812_A5
-#define JOY_RIGHT_Y_PORT ad7812_A6
+#define JOY_RIGHT_X_PORT ad7812_A6
+#define JOY_RIGHT_Y_PORT ad7812_A5
 
-#define JOY_LEFT_SW 9
-#define JOY_RIGHT_SW 10
+#define SW_L 17
+#define SW_R 18
 
 #define PAYLOAD_SIZE 6
 
@@ -51,18 +51,11 @@ psu_context_t * psu;
 
 char print_out[32] = {0};
 
-static void _partial_clear_oled(ushort x, ushort y, ushort width, ushort height)
-{
-    oled->color = SSD1306_COLOR_BLACK;
-    ssd1306_fill_rect(oled, x, y, width, height);
-    oled->color = SSD1306_COLOR_WHITE;
-}
-
 static nrf_context_t *_init_transmitter()
 {
     nrf_context_t *tx = nrf_init(NRF_MOSI, NRF_MISO, NRF_CLK, NRF_CS, NRF_CE);
 
-    //char output[21] = {0};
+    char output[21] = {0};
 
     byte a = nrf_set_primary_mode(tx, 0);
     byte b = nrf_enable_rx_pipes(tx, 0x00001);
@@ -74,12 +67,12 @@ static nrf_context_t *_init_transmitter()
     byte h = nrf_write_rx_pipe_address(tx, 0, rf_addr);
     byte i = nrf_write_tx_pipe_address(tx, rf_addr);
 
-    //sprintf(output, "%02x%02x%02x%02x%02x%02x%02x%02x%02x", a, b, c, d, e, f, g, h, i);
+    sprintf(output, "%02x%02x%02x%02x%02x%02x%02x%02x%02x", a, b, c, d, e, f, g, h, i);
 
-    //ssd1306_clear(oled);
-    //ssd1306_txt(oled, 0, 0, "TX initialized");
-    //ssd1306_txt(oled, 0, 16, output);
-    //ssd1306_display(oled);
+    ssd1306_clear(oled);
+    ssd1306_txt(oled, 0, 0, "TX initialized");
+    ssd1306_txt(oled, 0, 16, output);
+    ssd1306_display(oled);
 
     return tx;
 }
@@ -91,21 +84,21 @@ int main()
     oled = ssd1306_init(SSD1306_GEOMETRY_128_32, OLED_SCL, OLED_SDA);
     adc = ad7812_init(ADC_DIN, ADC_DOUT, ADC_CLK, ADC_CONVST);
     nrf = _init_transmitter();
+    pause(1000);
 
     vr_x_l = io_add_adc_binding(adc, JOY_LEFT_X_PORT, 0);
     vr_y_l = io_add_adc_binding(adc, JOY_LEFT_Y_PORT, 0);
     vr_x_r = io_add_adc_binding(adc, JOY_RIGHT_X_PORT, 0);
     vr_y_r = io_add_adc_binding(adc, JOY_RIGHT_Y_PORT, 0);
 
-    sw_l = io_add_switch_binding(JOY_LEFT_SW, 0);
-    sw_r = io_add_switch_binding(JOY_RIGHT_SW, 0);
+    sw_l = io_add_switch_binding(SW_L, 0);
+    sw_r = io_add_switch_binding(SW_R, 0);
 
     batt = io_add_adc_binding(adc, BATTERY_PORT, 0);
 
-    ssd1306_clear(oled);
-
     while (true)
     {
+        ssd1306_clear(oled);
         io_adc(batt);
         io_joystick(vr_x_l);
         io_joystick(vr_y_l);
@@ -115,54 +108,44 @@ int main()
         io_switch(sw_r);
 
         // left joystick
-        if (vr_x_l->value->newValueReady && vr_x_l->value->changed)
         {
             tx_dat[0] = vr_x_l->value->lastVal;
             vr_x_l->value->newValueReady = false;
         }
 
-        if (vr_y_l->value->newValueReady && vr_y_l->value->changed)
         {
             tx_dat[1] = vr_y_l->value->lastVal;
             vr_y_l->value->newValueReady = false;
         }
 
-        if (sw_l->value->newValueReady && sw_l->value->changed)
         {
             tx_dat[2] = sw_l->value->lastVal;
             sw_l->value->newValueReady = false;
         }
 
         // right joystick
-        if (vr_x_r->value->newValueReady && vr_x_r->value->changed)
         {
             tx_dat[3] = vr_x_r->value->lastVal;
             vr_x_r->value->newValueReady = false;
         }
 
-        if (vr_y_r->value->newValueReady && vr_y_r->value->changed)
         {
             tx_dat[4] = vr_y_r->value->lastVal;
             vr_y_r->value->newValueReady = false;
         }
 
-        if (sw_r->value->newValueReady && sw_r->value->changed)
         {
             tx_dat[5] = sw_r->value->lastVal;
             sw_r->value->newValueReady = false;
         }
 
-        if (batt->value->newValueReady && batt->value->changed)
+        //if (batt->value->newValueReady && batt->value->changed)
         {
-            ssd1306_clear(oled);
             ushort percent = psu_calculate_percent(psu, batt->value->lastVal);
 
             sprintf(print_out, "%d%%", percent);
             ssd1306_txt(oled, 0, 0, "Battery:");
             ssd1306_txt(oled, 128 - (strlen(print_out) * 7), 0, print_out);
-
-            sprintf(print_out, "%d %d %d", psu->adc_min, psu->adc_max, batt->value->lastVal);
-            ssd1306_txt(oled, 0, 16, print_out);
 
             batt->value->newValueReady = false;
         }
@@ -173,8 +156,9 @@ int main()
         else
             ssd1306_txt(oled, 121, 16, "-");
 
-        ssd1306_display(oled);
+        sprintf(print_out, "%02x%02x%02x%02x%02x%02x", tx_dat[0], tx_dat[1], tx_dat[2], tx_dat[3], tx_dat[4], tx_dat[5]);
+        ssd1306_txt(oled, 0, 16, print_out);
 
-        pause(100);
+        ssd1306_display(oled);
     }
 }
